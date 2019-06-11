8f653db945f496f2782799e4a87fe62b81e05630
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-06-11 20:15:06 -0300

    cap 4 ok

--- 1-navigate-between-pages/pages/index.js
+++ 1-navigate-between-pages/pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 
 const PostLink = props => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`} >
       <a>{props.title}</a>
     </Link>
   </li>
@@ -13,9 +13,9 @@ const Index = () => (
   <Layout>
     <h1>My Blog</h1>
     <ul>
-      <PostLink title="Hello Next.js" />
-      <PostLink title="Learn Next.js is awesome" />
-      <PostLink title="Deploy apps with Zeit" />
+      <PostLink id="hello-next" title="Hello Next.js" content="conteudo do post" />
+      <PostLink id="lear-next" title="Learn Next.js is awesome" content="conteudo do post 2"/>
+      <PostLink id="how-to-deploy" title="Deploy apps with Zeit" content="conteudo do post 3" />
     </ul>
   </Layout>
 )
